5b1d41d65da608f9a9b792fdf089743cd5f9c8f6
sardoraminov <oddiydasturchi@gmail.com>
2022-04-16 22:41:26 +0500

    design changed

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,120 +1,77 @@
 <template>
-  <div class="home bg-darkBlue min-h-screen flex justify-center items-center">
+  <div class="start-page min-h-screen">
     <div
-      class="register-box p-7 flex flex-col justify-between items-center w-full min-h-[470px] bg-lightBlue mx-3 sm:w-[580px] rounded shadow-black shadow-2xl"
+      class="register-form flex flex-col justify-evenly bg-blue min-h-[220px] text-center"
     >
-      <div class="titles">
-        <h1 class="text-white font-bold text-2xl mb-4">Assalomu alaykum!</h1>
-        <p class="text-white ">
-          Ismingiz orqali ro'yxatdan o'tib, o'zingizga kerakli testni
-          tanlashingiz mumkin.
-        </p>
-      </div>
-      <div class="inputs mb-8 w-full">
+      <span class="logo text-white font-light"
+        >livecoders <b class="font-extrabold text-xs">TM</b></span
+      >
+      <div class="inputs">
         <input
+          v-model="user.firstname"
+          class="mx-3 py-2 px-4 rounded-md outline-none text-white placeholder:text-[#D7F2FA] bg-transparent border-white border-2"
           type="text"
           placeholder="Ism"
-          class="w-full border bg-ultraLightBlue my-4 pl-5 py-2 pr-10 outline-none rounded text-white placeholder:text-gray-300"
         />
-        <Listbox as="div" v-model="selected">
-          <div class="mt-1 relative">
-            <ListboxButton
-              class="relative border cursor-pointer w-full bg-ultraLightBlue rounded pl-3 pr-10 py-2 text-left focus:outline-none focus:ring-1 sm:text-sm"
-            >
-              <span class="flex items-center">
-                <span class="ml-3 block text-white truncate">{{
-                  selected
-                }}</span>
-              </span>
-            </ListboxButton>
-
-            <transition
-              leave-active-class="transition ease-in duration-100"
-              leave-from-class="opacity-100"
-              leave-to-class="opacity-0"
-              enter-active-class="transition ease-in duration-100"
-            >
-              <ListboxOptions
-                class="absolute z-10 mt-1 w-full bg-ultraLightBlue shadow-lg max-h-56 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm"
-              >
-                <ListboxOption
-                  as="template"
-                  v-for="category in categories"
-                  :key="category"
-                  :value="category"
-                  v-slot="{ active, selected }"
-                  class="cursor-pointer"
-                >
-                  <li
-                    :class="[
-                      active ? 'text-white bg-darkBlue' : 'text-gray-900',
-                      'cursor-default select-none relative py-2 pl-3 pr-9',
-                    ]"
-                  >
-                    <div class="flex items-center">
-                      <span
-                        :class="[
-                          selected ? 'font-bold' : 'font-normal',
-                          'ml-3 block truncate text-white',
-                        ]"
-                      >
-                        {{ category }}
-                      </span>
-                    </div>
-                  </li>
-                </ListboxOption>
-              </ListboxOptions>
-            </transition>
-          </div>
-        </Listbox>
-      </div>
-      <div class="button w-full">
-        <button
-          class="btn bg-darkBlue rounded transition-all hover:-translate-y-1 hover:shadow-lg text-white uppercase font-bold w-full p-4"
-        >
-          boshlash
-        </button>
+        <input
+          v-model="user.lastname"
+          class="mx-3 py-2 px-4 rounded-md outline-none text-white placeholder:text-[#D7F2FA] bg-transparent border-white border-2"
+          type="text"
+          placeholder="Familiya"
+        />
       </div>
     </div>
+    <main class="subjects-box p-4">
+      <h1 class="title text-dark font-bold text-xl">Testlar</h1>
+      <div class="subjects grid grid-cols-auto">
+        <router-link to="#"
+          class="subject my-4 border"
+          v-for="(subject, index) in subjects"
+          :key="subject.id"
+        >
+            <p class="subject-id">0{{ index + 1 }}</p>
+            <h2 class="subject-name">{{ subject.name }}</h2>
+        </router-link>
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
-import { ref, watch } from "vue";
-import {
-  Listbox,
-  ListboxButton,
-  ListboxLabel,
-  ListboxOption,
-  ListboxOptions,
-} from "@headlessui/vue";
-const categories = [
-  "Tarix",
-  "Ingliz tili",
-  "Informatika",
-  "Category",
-  "Random1",
-  "Random2",
-  "Random3",
-  "Random4",
-  "Random5",
-];
+import { computed, reactive, watch } from "vue";
 export default {
-  name: "HomeView",
-  components: {
-    Listbox,
-    ListboxButton,
-    ListboxLabel,
-    ListboxOption,
-    ListboxOptions,
-  },
+  name: "Home",
   setup() {
-    const selected = ref(categories[1]);
+    const user = reactive({
+      firstname: "",
+      lastname: "",
+      fullname: computed(() => `${user.firstname} ${user.lastname}`),
+    });
+
+    const subjectsArr = [
+      { id: 1, name: "Tarix", active: false },
+      { id: 2, name: "Ingliz tili", active: true },
+      { id: 3, name: "Matematika", active: false },
+    ];
+
+    const sortedSubjects = computed(() =>
+      subjectsArr.sort((a, b) => Number(b.active) - Number(a.active))
+    );
+    const subjects = reactive(sortedSubjects);
+
+    watch(
+      () => user.fullname,
+      (newValue) => {
+        console.log(newValue);
+      }
+    );
 
     return {
-      categories,
-      selected,
+      user,
+      subjects,
     };
   },
 };
 </script>
+
+<style></style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,17 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ["Montserrat", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
       },
       colors: {
-        darkBlue: "#001219",
-        lightBlue: "#00435D",
-        green: "#2DC653",
-        ultraLightBlue: "#1F5A71",
+        green: "#9BC53D",
+        blue: "#54C6EB",
+        gray: "#E2E2E2",
+        dark: "#202020",
       },
+      gridTemplateColumns: {
+        auto: "repeat(auto-fit, minmax(200px, 1fr))",
+      }
     },
   },
   plugins: [],
