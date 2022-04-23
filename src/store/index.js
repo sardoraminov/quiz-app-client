1e10c38c3b88de2bec8e9cd969e50fbacb6026d2
sardoraminov <oddiydasturchi@gmail.com>
2022-04-24 00:51:19 +0500

    some changes here

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,43 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
+import { api } from "../plugins/api";
 
 export default createStore({
   state: {
+    exams: [],
+    loading: false,
+    error: null,
+    currentExam: {},
   },
   getters: {
+    getExams: (state) => state.exams,
+    getLoading: (state) => state.loading,
+    getCurrentExam: (state) => state.currentExam,
   },
   mutations: {
+    setExams(state, payload) {
+      state.exams = payload;
+    },
+    setLoading(state, payload) {
+      state.loading = payload;
+    },
+    setCurrentExam(state, payload) {
+      state.currentExam = payload;
+    }
   },
   actions: {
+    async fetchExams({ commit }, payload) {
+      commit("setLoading", true);
+      try {
+        const response = await api.get(`/exams`);
+        commit("setExams", response.data);
+        commit("setLoading", false);
+        console.log(response.data);
+      } catch (error) {
+        commit("setLoading", false);
+        commit("setError", error.message);
+        console.log(error);
+      }
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -21,44 +21,15 @@
         />
       </div>
     </div>
-    <div class="text-center my-6 text-lg mt-8" v-if="user.firstname.length < 2 || user.lastname.length < 5">
+    <div
+      class="text-center my-6 text-lg mt-8"
+      v-if="user.firstname.length < 2 || user.lastname.length < 5"
+    >
       Ism familiyangizni to'liq yozing!
     </div>
     <main v-else class="subjects-box py-6 px-7">
       <h1 class="title text-dark font-bold text-xl">Testlar</h1>
-      <div class="subjects grid grid-cols-auto relative gap-12 my-4 mt-11">
-        <router-link
-          v-for="(subject, index) in subjects"
-          :key="subject.id"
-          :to="
-            subject.active
-              ? `/test?subject=${subject.name}&pupil=${user.fullname}`
-              : '#' + subject.id
-          "
-          :class="[
-            subject.active ? 'border-green' : 'border-gray',
-            'subject relative border-[5px] rounded-md h-[230px] flex justify-center items-center',
-          ]"
-        >
-          <p
-            :class="[
-              subject.active ? 'bg-green' : 'bg-gray',
-              'subject-id font-bold absolute -top-3  -left-3 text-white px-3 py-[1.5px] rounded-full',
-            ]"
-          >
-            0{{ index + 1 }}
-          </p>
-          <h2
-            :class="[
-              subject.active ? 'text-green' : 'text-gray',
-              'text-2xl font-bold',
-            ]"
-          >
-            {{ subject.name }}
-          </h2>
-        </router-link>
-      </div>
-      <div class="reminder mt-12">
+      <div class="reminder my-6">
         <div class="box flex flex-row items-center my-2">
           <div class="something available bg-green w-5 h-5 mr-2"></div>
           - Mavjud
@@ -68,15 +39,50 @@
           - Mavjud emas
         </div>
       </div>
+      <div class="subjects grid grid-cols-auto relative gap-12 my-4 mt-11">
+        <router-link
+          v-for="(exam, index) in exams"
+          :key="index"
+          :to="
+            exam.active
+              ? `/test?subject=${exam.name}&pupil=${user.fullname}`
+              : '#' + index
+          "
+          :class="[
+            exam.active ? 'border-green' : 'border-gray',
+            'subject relative border-[5px] rounded-md h-[230px] flex justify-center items-center',
+          ]"
+        >
+          <p
+            :class="[
+              exam.active ? 'bg-green' : 'bg-gray',
+              'subject-id font-bold absolute -top-3  -left-3 text-white px-3 py-[1.5px] rounded-full',
+            ]"
+          >
+            0{{ index + 1 }}
+          </p>
+          <h2
+            :class="[
+              exam.active ? 'text-green' : 'text-gray',
+              'text-2xl font-bold',
+            ]"
+          >
+            {{ exam.name }}
+          </h2>
+        </router-link>
+      </div>
     </main>
   </div>
 </template>
 
 <script>
-import { computed, reactive, ref } from "vue";
+import { computed, onMounted, reactive, ref, watch } from "vue";
+import { useStore } from "vuex";
 export default {
   name: "Home",
   setup() {
+    const store = useStore();
+
     const user = reactive({
       firstname: "",
       lastname: "",
@@ -85,21 +91,21 @@ export default {
 
     const isFullname = ref(user.fullname.length > 2 ? true : false);
 
-    const subjectsArr = [
-      { id: 1, name: "Tarix", active: false },
-      { id: 2, name: "Ingliz tili", active: true },
-      { id: 3, name: "Matematika", active: false },
-    ];
 
-    const sortedSubjects = computed(() =>
-      subjectsArr.sort((a, b) => Number(b.active) - Number(a.active))
-    );
-    const subjects = reactive(sortedSubjects);
+    onMounted(() => {
+      setTimeout(() => {
+        store.dispatch("fetchExams");
+      }, 1000);
+    });
+
+    const exams = computed(() => {  
+      return store.getters.getExams;
+    });
 
     return {
       user,
-      subjects,
       isFullname,
+      exams,
     };
   },
 };
